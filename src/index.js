3418568944172b6383470be492344e5fd8c74f49
KingHenryJr <henryyihengzhang@gmail.com>
2018-05-15 16:39:51 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@ import ReactDOM from 'react-dom';
 
 import Spaceship from './components/Spaceship';
 
+const colors = ['yellow', 'pink']
+
+Spaceship.defaultProps = {
+  name: "SpeedRacer",
+  speed: "slow",
+  hasRockets: false,
+  color: ['black', 'red']
+}
+
 ReactDOM.render(
-  <Spaceship name="Millennium Falcon" />,
+  <div>
+    <Spaceship
+      name="Millennium F"
+      speed = "5"
+      hasRockets = "true"
+      color = {colors}
+    />
+  </div>,
   document.getElementById('global')
 );
